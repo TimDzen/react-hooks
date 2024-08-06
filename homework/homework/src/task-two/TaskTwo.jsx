f8ebc375e37531a5b2c2a6faf9fff35c4f99360f
TimDzen <140600844+TimDzen@users.noreply.github.com>
2024-08-06 22:10:01 +0300

    Update TaskTwo.jsx

--- homework/homework/src/task-two/TaskTwo.jsx
+++ homework/homework/src/task-two/TaskTwo.jsx
@@ -1,55 +1,46 @@
-import React, { useCallback, useState } from 'react'
+import React, { useCallback, useState, useRef } from 'react';
 import RenderCounter from './render-counter/RenderCounter';
 import './TaskTwo.css';
 
 export default function TaskTwo() {
-    const update = useUpdate()
+    const update = useUpdate();
     return (
         <div className="TaskTwo">
             <button onClick={update}>Обновить компонент</button>
-            {<RenderCounter />}
+            <RenderCounter />
             <Root />
         </div>
-    )
+    );
 }
 
-const Root = React.memo(() => { // React.memo предотвратит перерендеринг, когда пропсы и state не изменились
+const Root = React.memo(() => {
     const [value, setValue] = useState('');
 
-    // Используем useCallback, чтобы функция не создавалась заново при каждом рендере
     const handleChange = useCallback((event) => {
-        setValue(event.target.value)
+        setValue(event.target.value);
     }, []);
 
     return (
         <form className="form-container">
             Введенное значение: {value}
-            {<RenderCounter />}
+            <RenderCounter />
             <Input onChange={handleChange} />
         </form>
-    )
+    );
 });
 
-// Враппим компонент Input с React.memo для предотвращения его перерисовки при каждом рендере Root
 const Input = React.memo(({ onChange }) => {
     return (
         <div className="input-container">
             <input type="text" className="input-field" name="value" onChange={onChange} />
-            {<RenderCounter />}
+            <RenderCounter />
         </div>
-    )
+    );
 });
 
 function useUpdate() {
     const [, setCount] = useState(0);
-    const update = useCallback(() => { // Использование useCallback здесь означает, что функция будет одна и та же между рендерами
-        setCount(c => c + 1)
+    return useCallback(() => {
+        setCount(c => c + 1);
     }, []);
-
-    return update;
 }
-
-// Root обернут в React.memo, что предотвратит его перерендер, если пропсы и состояния не изменились.
-// handleChange обернут в useCallback с пустым массивом зависимостей, это означает, что ссылка на функцию будет сохраняться между рендерами.
-// Input тоже обернут в React.memo, теперь компонент не будет перерендериваться, если его пропсы не изменились.
-// useUpdate теперь возвращает функцию, обернутую в useCallback, чтобы она не менялась и не вызывала перерендеры тех компонентов, функции которых зависят от useUpdate.
